3f5c4664352ded2888438aedd3fd1ba31200c211
Theo-D <theodessaigne@hotmail.fr>
2025-02-14 20:26:20 +0100

    added a program that prints every combination of every base 10 * single digits separated by ", "

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+*main - cycle through every digit from 0 to 9
+*
+* Return: prints every combination of every base 10 
+* single digits separated by ", "
+*/
+int main(void)
+{
+	int asciinubmers;
+	int asciinubmers2;
+
+	for (asciinubmers = 48 ; asciinubmers < 58 ; asciinubmers++)
+	{
+		for (asciinubmers2 = 48 ; asciinubmers2 < 58 ; asciinubmers2++)
+		{
+				putchar(asciinubmers);
+				putchar(asciinubmers2);
+				if (asciinubmers != 57 || asciinubmers2 != 57) /*Pourquoi || et pas && ?*/
+				{
+					putchar(',');
+					putchar(' ');
+				}
+		}
+		
+	}
+	putchar('\n');
+	return (0);
+}
