237d341786f60dbdc04afffc73474830ecd6e298
Theo-D <theodessaigne@hotmail.fr>
2025-02-14 19:44:50 +0100

    Added a program that prints every base16 digits

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+*main - cycles through every base16 digits
+*
+* Return: Every base16 digits
+*/
+int main(void)
+{
+	int asciiLetter;
+
+	for (asciiLetter = 48 ; asciiLetter < 58 ; asciiLetter++)
+	{
+		putchar(asciiLetter);
+	}
+	for (asciiLetter = 65 ; asciiLetter < 71 ; asciiLetter++)
+	{
+		putchar(asciiLetter);
+	}
+	putchar('\n');
+	return (0);
+}
