e263246e9e95b78275ba6ee2e32b3897e5aefbe1
Theo-D <theodessaigne@hotmail.fr>
2025-02-14 16:23:48 +0100

    added a space between if and parenthesis

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -19,11 +19,11 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	mod = n % 10;
 
-	if(mod > 5)
+	if (mod > 5)
 	{
 		printf("Last digit of %d is %d and is greater than 5\n", n, mod);
 	}
-	else if(mod == 0)
+	else if (mod == 0)
 	{
 		printf("Last digit of %d is %d and is 0\n", n, mod);
 	}
