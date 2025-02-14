3420f8bb4815d30cbbfb03a18b2217fcf9258a5e
Theo-D <theodessaigne@hotmail.fr>
2025-02-14 19:04:25 +0100

    Added a program that prints every letter of the alpahbet except for e and q. Updated README.md accordingly

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+*main - cycles through letter from a to z
+*
+* Return: prints each letter a the alphabet
+* except e and q.
+*/
+int main(void)
+{
+	int asciiLetter;
+
+	for (asciiLetter = 97 ; asciiLetter < 123 ; asciiLetter++)
+	{
+		if(asciiLetter != 101 && asciiLetter != 113)
+		{	
+			putchar(asciiLetter);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
