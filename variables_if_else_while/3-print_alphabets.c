5c1d292b760f5659c668ef3d0ef83ea04eb4ae0b
Theo-D <theodessaigne@hotmail.fr>
2025-02-14 16:56:11 +0100

    Added a program that prints the alphabet in lowercase, and then in uppercase, followed by a new line. and updated README.md accordingly

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+*main - cycles through letter from a to z
+*
+* Return: each letter a the alphabet
+*/
+int main(void)
+{
+	int asciiLetter;
+
+	for (asciiLetter = 97 ; asciiLetter < 123 ; asciiLetter++)
+	{
+		putchar(asciiLetter);
+	}
+	for (asciiLetter = 65 ; asciiLetter < 91 ; asciiLetter++)
+        {
+                putchar(asciiLetter);
+        }
+	putchar('\n');
+	return (0);
+}
