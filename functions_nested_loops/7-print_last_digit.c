858d5d6d03fe6d9e63cd9fe1ddb2711902186cb3
Theo-D <theodessaigne@hotmail.fr>
2025-02-17 20:52:01 +0100

    Fixed the code to conform to betty style

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- *_abs - Computes the absolute value of an integer
- * @n: int to be put to abs
+ *print_last_digit - Displays the last digit of given number
+ * @n: int of which the last digit will be displayed
  *
- * Return: An integer to its absolute value
+ * Return: Last digit of the int used as parameter
  */
 
 int print_last_digit(int n)
@@ -14,14 +14,14 @@ int print_last_digit(int n)
 	if (n >= 0)
 	{
 		mod = n % 10;
-                _putchar(mod + 48);
-		return (mod);	
+		_putchar(mod + 48);
+		return (mod);
 	}
 	else
 	{
 		n *= -1;
 		mod = n % 10;
 		_putchar(mod + 48);
-		return(mod);
+		return (mod);
 	}
 }
