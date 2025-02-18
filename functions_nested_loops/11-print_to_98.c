d2a6e38b5dc85f37e33c31dc7f10247bc5c0614f
Theo-D <theodessaigne@hotmail.fr>
2025-02-18 22:17:43 +0100

    Added description for function, args and return

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -2,8 +2,8 @@
 #include <stdio.h>
 /**
  *print_to_98 - prints every natural numbers from given number to 98
- *@n: The natural number from which to start the count to 98 
-
+ *@n: The natural number from which to start the count to 98
+ *
  * Return: Every natural number from n to 98
  */
 void print_to_98(int n)
