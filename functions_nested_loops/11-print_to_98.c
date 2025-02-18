4c0f2d4e200bbcc2103e78927d491a61b651e98f
Theo-D <theodessaigne@hotmail.fr>
2025-02-18 22:12:59 +0100

    Replaced blank spaces by tabs to conform to betty style

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -10,7 +10,7 @@ void print_to_98(int n)
 {
 	if (n <= 98)
 	{
-		for(; n <= 98; n++ )
+		for (; n <= 98; n++)
 		{
 			if (n == 98)
 				printf("%d", n);
@@ -26,7 +26,7 @@ void print_to_98(int n)
 			if (n == 98)
 				printf("%d", n);
 			else
-                        	printf("%d, ", n);
+				printf("%d, ", n);
 		}
 	printf("\n");
 	}
