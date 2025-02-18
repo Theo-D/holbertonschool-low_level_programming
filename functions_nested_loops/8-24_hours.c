7c578c477dcbd8eaf1d6ac6dbfea1dffbe2aaca6
Theo-D <theodessaigne@hotmail.fr>
2025-02-18 11:25:41 +0100

    Removed trailing white spaces to conform to betty style

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -2,10 +2,10 @@
 
 /**
  *jack_bauer - Loops through every numbers from 0 to 59
- * and every numbers from 0 to 23 and prints them after 
+ * and every numbers from 0 to 23 and prints them after
  * parsing
  *
- * Return: every 
+ * Return: every minutes within a day to the format hh:mm
  */
 
 void jack_bauer(void)
