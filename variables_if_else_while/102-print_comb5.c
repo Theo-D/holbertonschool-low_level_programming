61ab027448fcaa4e97178b3ce976bb05937b22af
Theo-D <theodessaigne@hotmail.fr>
2025-02-19 15:13:38 +0100

    modified the file t oconform to betty style

--- variables_if_else_while/102-print_comb5.c
+++ variables_if_else_while/102-print_comb5.c
@@ -14,14 +14,14 @@ int main(void)
 
 	for (number1 = 0; number1 <= 98; number1++)
 	{
-		for ( number2 = number1 + 1; number2 <= 99; number2++)
+		for (number2 = number1 + 1; number2 <= 99; number2++)
 		{
 			putchar((number1 / 10) + 48);
 			putchar((number1 % 10) + 48);
 			putchar(' ');
 			putchar((number2 / 10) + 48);
 			putchar((number2 % 10) + 48);
-			if(number1 != 98 || number2 != 99)
+			if (number1 != 98 || number2 != 99)
 			{
 				putchar(',');
 				putchar(' ');
