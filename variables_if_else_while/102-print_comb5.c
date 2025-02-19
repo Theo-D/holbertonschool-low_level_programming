f130f6f6155a92ff603c5dcbb7502da42fdae4f4
Theo-D <theodessaigne@hotmail.fr>
2025-02-19 15:12:08 +0100

    a program that prints all possible combinations of two two-digit numbers. Updated README.md accordingly

---
+++ variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+*main - cycle through every digit from 0 to 9
+*
+* Return: prints every combination of every base 10
+* single digits separated by ", "
+*/
+int main(void)
+{
+	int number1, number2;
+
+	for (number1 = 0; number1 <= 98; number1++)
+	{
+		for ( number2 = number1 + 1; number2 <= 99; number2++)
+		{
+			putchar((number1 / 10) + 48);
+			putchar((number1 % 10) + 48);
+			putchar(' ');
+			putchar((number2 / 10) + 48);
+			putchar((number2 % 10) + 48);
+			if(number1 != 98 || number2 != 99)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
+
