e1ff80b262c877417e8e56cd6eb86f2d5b20f352
Theo-D <theodessaigne@hotmail.fr>
2025-02-24 11:11:55 +0100

    Added 5-more_numbers that contains the definition for more_numbers() that prints every number from 1 to 14 10 times. Updated main.h to inclue the declaration for more_numbers(). Updated README.md accordingly

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ *more_numbers - Prints numbers from 1 to 14 ten times
+ *
+ *Return: every number from 1 to 14 ten times
+ */
+
+void more_numbers(void)
+{
+	int i;
+	int j;
+
+	for (j = 0; j < 10; j++)
+	{
+		for (i = 0; i <= 14; i++)
+		{
+			if (i > 9)
+			{
+				_putchar((i / 10) + 48);
+			}
+			_putchar((i % 10) + 48);
+		}
+		_putchar('\n');
+	}
+}
--- more_functions_nested_loops/README.md
+++ more_functions_nested_loops/README.md
@@ -3,3 +3,4 @@
 2-mul - multiplies two integers.
 3-print_numbers - Print every number from 0 to 9.
 4-print_most_numbers - every numbers from 0 to 9 except for 2 and 4.
+5-more_numbers - Prints every number from 1 to 14 ten times.
