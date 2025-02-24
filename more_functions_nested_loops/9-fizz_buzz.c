c63d4a5f263395dd115fbedfaccde096a5636862
Theo-D <theodessaigne@hotmail.fr>
2025-02-24 13:19:10 +0100

    Fixed code format to comply to betty style and added documentation

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -1,19 +1,24 @@
 #include <stdio.h>
 
 /**
+ *main - prints the numbers from 1 to 100, followed by a new line. But for
+ *multiples of three print Fizz instead of the number and for the multiples
+ *of five print Buzz. For numbers which are multiples of both three and
+ *five print FizzBuzz.
  *
+ *Return: Always 0
  */
 
 int main(void)
 {
 	int i;
-	
+
 	for (i = 1; i <= 100; i++)
 	{
-		if ((i % 5) == 0 && (i % 3) == 0)
-                {
-                        printf("FizzBuzz");
-                }
+		if (i % 5 == 0 && i % 3 == 0)
+		{
+			printf("FizzBuzz");
+		}
 		else if (i % 5 == 0)
 		{
 			printf("Buzz");
@@ -26,11 +31,11 @@ int main(void)
 		{
 			printf("%d", i);
 		}
-		if(i != 100)
+		if (i != 100)
 		{
 			printf(" ");
 		}
 	}
-	printf("%c",'\n');
+	printf("%c", '\n');
 	return (0);
 }
