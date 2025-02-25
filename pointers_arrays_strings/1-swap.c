e968bec9a696b03a391af02b96da876cb4ca63bd
Theo-D <theodessaigne@hotmail.fr.>
2025-02-25 23:57:00 +0100

    Removed trailing white spaces to conform to betty style and added documentation for the function.

--- pointers_arrays_strings/1-swap.c
+++ pointers_arrays_strings/1-swap.c
@@ -1,7 +1,11 @@
 #include "main.h"
 
 /**
+ *swap_int - Swaps two integers given as parameters
+ *@a: first integer to be swaped.
+ *@b: second integer to swaped.
  *
+ *Return: Swaped value of both integers given as parameters
  */
 
 void swap_int(int *a, int *b)
@@ -18,12 +22,12 @@ void swap_int(int *a, int *b)
 	 * int ValueA;
 	 * valueA = *a;
 	 * *b = valueA;
-	 * 
+	 *
 	 * fonctionne?
 	 */
 	int valueA;
 	int valueB;
-	
+
 	valueA = *a;
 	valueB = *b;
 	*b = valueA;
