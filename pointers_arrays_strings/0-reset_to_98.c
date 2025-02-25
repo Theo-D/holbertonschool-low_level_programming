a9dbfc88148ea10b565f45b158a2088d7d6446f7
root <root@DESKTOP-JVJDEPC.>
2025-02-25 19:52:58 +0100

    Added 0-reset_to_98.c that contains the definition for reset_to_98() that takes a pointer to an int as parameter and updates the value it points to to 98. Added main.h to include the declaration for reset_to_98(). Added README.md to include a description for reset_to_98.c

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ *reset_to_98 - a function that takes a pointer to an int
+ *as parameter and updates the value it points to to 98.
+ *@n: pointer to an int to be reseted to 98.
+ *
+ *Return: 98, hopefuly.
+ */
+
+void reset_to_98(int *n)
+{
+	*n =98;
+}
