457a361b96c0d0e898401c1d118a39d929e125bd
Theo-D <theodessaigne@hotmail.fr>
2025-02-26 20:00:46 +0100

    Modified the index to include the null character

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -1,17 +1,22 @@
 #include "main.h"
 
 /**
+ * _strcpy - Copies a string from a src to a dest.
+ * @dest: destination of the string to be copied.
+ * @src: source of the string to be copied.
  *
+ * Return: The destination string with de copied content from src
  */
 
 char *_strcpy(char *dest, char *src)
 {
 	int i = 0;
 
-	while(src[i])
+	while (src[i])
 	{
 		dest[i] = src[i];
 		i++;
 	}
+	dest[i + 1] = src[i + 1];
 	return (dest);
 }
