7485b4c0554a444857b920c13e2233a19c16f3ee
Theo-D <theodessaigne@hotmail.fr>
2025-02-27 11:20:41 +0100

    Added description for the function

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -1,7 +1,11 @@
 #include "main.h"
 
 /**
+ * _strcat - A function that concatenates two given strings.
+ * @dest: The first part of the string.
+ * @src: The string to append to the first string.
  *
+ * Return: A string made of the two strings given as param.
  */
 
 char *_strcat(char *dest, char *src)
