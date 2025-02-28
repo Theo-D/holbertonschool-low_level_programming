187e0c5825169c6407aff94c29941445f9852c7f
Theo-D <theodessaigne@hotmail.fr>
2025-02-28 12:13:01 +0100

    Added description for the function

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -1,7 +1,11 @@
 #include "main.h"
 
 /**
+ * _strcmp - A function that compares two strings
+ * @s1: First string to be compared.
+ * @s2: Second string to be compared.
  *
+ * Return: The difference between the two given strings.
  */
 
 int _strcmp(char *s1, char *s2)
