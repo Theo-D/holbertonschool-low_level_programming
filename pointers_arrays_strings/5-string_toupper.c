7a9aaabae9a18c8c9d0788365ada9ff901a1fea0
Theo-D <theodessaigne@hotmail.fr>
2025-02-28 15:57:24 +0100

    Added description for the function and modified the code to conform to betty style.

--- pointers_arrays_strings/5-string_toupper.c
+++ pointers_arrays_strings/5-string_toupper.c
@@ -1,14 +1,17 @@
 #include "main.h"
 
 /**
+ * string_toupper - changes all lowercase letters of a string to uppercase.
+ * @c: string to be modified to uppercase
  *
+ * Return: The given string uppercase.
  */
 
 char *string_toupper(char *c)
 {
 	int i = 0;
 
-	while(c[i] != 0)
+	while (c[i] != 0)
 	{
 		if (c[i] >= 97 && c[i] <= 122)
 			c[i] = c[i] - 32;
