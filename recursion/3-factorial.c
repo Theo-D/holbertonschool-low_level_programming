ebc95c73f838ed75878c950ffc2758823cab803f
Theo-D <theodessaigne@hotmail.fr>
2025-03-04 11:22:20 +0100

    Added description for factorial().

--- recursion/3-factorial.c
+++ recursion/3-factorial.c
@@ -1,7 +1,11 @@
 #include "main.h"
 
 /**
+ *factorial - Prints the factorials of a given number.
+ *@n: The number from which the factorials should be printed.
  *
+ *Return: -1 of n < 0, 1 if n = 0 or 1. Otherwise, return the
+ *factorial of the given number.
  */
 
 int factorial(int n)
