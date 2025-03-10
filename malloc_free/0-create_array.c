7e8e389f1252393860fba97a4da726cdf5ede5b3
Theo-D <theodessaigne@hotmail.fr>
2025-03-10 13:58:56 +0100

    Added 0-create_array.c that contains the definition for create_array() that creates an array of chars, and initializes it with a specific char. Updated main.h to include the declaration for create_array(). Updated README.md accordingly.x

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ *create_array - creates an array of chars, and
+ *initializes it with a specific char.
+ *@size: The size of the array
+ *@c: the character to populate the array.
+ *
+ *Return: if size == 0 or if function fails, return NULL,
+ *else returns pointer to the array created.
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *arr = (char *)malloc(size * sizeof(c));
+	unsigned int i = 0;
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	else
+	{
+		while (i < size)
+		{
+			arr[i] = c;
+			i++;
+		}
+		return (arr);
+	}
+
+}
