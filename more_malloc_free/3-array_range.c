cc66acd34034ba6b4d4f77e00dfde71f39325904
Theo-D <theodessaigne@hotmail.fr>
2025-03-11 18:10:25 +0100

    Added 3-array_range.c that contains the definition for array_range() that  creates an array of integers. Updated main.h to include the declaration for array_range(). Updated README.md accordingly.

---
+++ more_malloc_free/3-array_range.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ *array_range - creates an array of integers.
+ *@min: start value of the array.
+ *@max: end value of the array.
+ *
+ *Return: Null if min > max or if malloc fails. Else, return array.
+ */
+
+int *array_range(int min, int max)
+{
+	int *arr;
+	int diff = max - min;
+	int i;
+
+	if (min > max)
+		return (NULL);
+
+	arr = malloc(diff  * sizeof(int));
+	if (arr == NULL)
+		return (NULL);
+
+	for (i = min; i < max; i++)
+	{
+		arr[i] = i;
+	}
+	return (arr);
+}
--- more_malloc_free/README.md
+++ more_malloc_free/README.md
@@ -1,2 +1,4 @@
 0-malloc_checked.c - allocates memory using malloc.
 1-str_nconcat.c - concatenates two strings.
+2-calloc.c - allocates memory for an array, using malloc.
+3-array_range.c - creates an array of integers.
