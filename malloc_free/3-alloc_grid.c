4f8e866778f721d58f624dcbd09f407938cf26b0
Theo-D <theodessaigne@hotmail.fr>
2025-03-11 10:27:45 +0100

    Added 3-alloc_grid.c that contains the definition for alloc_gri() that returns a pointer to a 2 dimensional array of integers. Updated main.h to include the declaration for alloc_grid(). Updated README.md accordingly.

---
+++ malloc_free/3-alloc_grid.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ *alloc_grid - returns a pointer to a 2 dimensional array of integers.
+ *@width: the number of columns in the array
+ *@height: the number of rows in the array
+ *
+ *Return: the 2 dimensionnal array, else if width and/or height are <= 0
+ *return NULL.
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int **arr;
+	int i, j;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+
+	arr = malloc(height * sizeof(int));
+
+	for (i = 0; i < height; i++)
+	{
+		arr[i] = malloc(width * sizeof(int));
+		for (j = 0; j < width; j++)
+		{
+			arr[i][j] = 0;
+		}
+	}
+	return (arr);
+}
