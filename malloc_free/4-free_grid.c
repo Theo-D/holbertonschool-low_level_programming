a10677094539fd0251ea6f7fddd58c83f6422716
Theo-D <theodessaigne@hotmail.fr>
2025-03-11 14:00:46 +0100

    Added doc for the function

--- malloc_free/4-free_grid.c
+++ malloc_free/4-free_grid.c
@@ -2,7 +2,11 @@
 #include <stdlib.h>
 
 /**
+ *free_grid - Frees a grid previously created in alloc_array()
+ *@grid: The grid to be freed.
+ *@height: The height of the given grid.
  *
+ *Return: The grid, freed.
  */
 
 void free_grid(int **grid, int height)
