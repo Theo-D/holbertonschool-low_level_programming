77eba59786bfebaafe8d101ac79c8c4f174d03e1
Theo-D <theodessaigne@hotmail.fr>
2025-03-11 11:14:10 +0100

    Added handling of malloc error and loop for freeing arr if index is NULL

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -20,9 +20,20 @@ int **alloc_grid(int width, int height)
 
 	arr = malloc(height * sizeof(int *));
 
+	if (arr == NULL)
+		return (NULL);
+
 	for (i = 0; i < height; i++)
 	{
 		arr[i] = malloc(width * sizeof(int));
+		if (arr[i] == NULL)
+		{
+			for (i = i - 1; i >= 0; i--)
+			{
+				free(arr[i]);
+
+			}
+		}
 		for (j = 0; j < width; j++)
 		{
 			arr[i][j] = 0;
