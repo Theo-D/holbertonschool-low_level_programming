85e429863d19a09af99b6b15f6222a821b328028
Theo-D <theodessaigne@hotmail.fr>
2025-03-11 09:34:59 +0100

    Added 2-str_concat.c that contains the definition for str_concat() that concatenates two strings. Updated main.h to include the declaration for str_concat(). Updated README.md accordingly.

---
+++ malloc_free/2-str_concat.c
@@ -0,0 +1,49 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ *str_concat - a function that concatenates two strings.
+ *@s1: First string to concatenate.
+ *@s2: Second string to concatenate.
+ *
+ *Return: Null on failure or and array containing the concatenated strings.
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	unsigned int sizeS1 = 0, sizeS2 = 0;
+	unsigned int i, j;
+	char *arr;
+
+	if (s1 == NULL || s2 == NULL)
+		return (NULL);
+
+	while (s1[sizeS1])
+	{
+		sizeS1++;
+	}
+
+	while (s2[sizeS2])
+	{
+		sizeS2++;
+	}
+
+	arr = malloc((sizeS1 + sizeS2) + 1 * sizeof(char));
+
+	if (arr == NULL)
+		return (NULL);
+
+	for (i = 0; i < sizeS1; i++)
+	{
+		arr[i] = s1[i];
+	}
+
+	for (j = sizeS1; j < (sizeS1 + sizeS2); j++)
+	{
+		arr[j] = *s2;
+		s2++;
+	}
+	arr[j] = 0;
+
+	return (arr);
+}
