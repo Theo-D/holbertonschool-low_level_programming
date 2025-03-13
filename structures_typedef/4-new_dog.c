66dacb38b5e5f7719cf21769d8278576dac9afcf
Theo-D <theodessaigne@hotmail.fr>
2025-03-13 14:22:40 +0100

    Added 4-new_dog.c that contains the definition for new_dog() that creates a new dog. Updated dog.h to include the declaration for new_dog(). Updated README.md with a short description of 4-new_dog.c

---
+++ structures_typedef/4-new_dog.c
@@ -0,0 +1,71 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _strlen - Return length of a given string.
+ * @s: the given string.
+ *
+ * Return: an int reprsenting the length of the string.
+ */
+
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i] != 0)
+		i++;
+	return (i);
+}
+
+/**
+ * _strcpy - Copies a string to another.
+ * @s1: destination string.
+ * @s2: source string.
+ *
+ * Return: The copied string.
+ */
+
+char *_strcpy(char *s1, char *s2)
+{
+	int i;
+
+	for (i = 0; s2[i] != 0; i++)
+	{
+		s1[i] = s2[i];
+	}
+	s1[i] = 0;
+	return (s1);
+}
+
+/**
+ * new_dog - Creates a new object dog.
+ * @name: The name of the new dog.
+ * @age: the age of the new dog.
+ * @owner: the owner of the new dog.
+ *
+ * Return: The new dog.
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	struct dog *newDog;
+
+	newDog = malloc(sizeof(struct dog));
+	if (newDog == NULL)
+		return (NULL);
+
+	newDog->name = malloc(_strlen(name) + 1 * sizeof(char));
+	if (newDog->name == NULL)
+		return (NULL);
+
+	newDog->owner = malloc(_strlen(owner) + 1 * sizeof(char));
+	if (newDog->owner == NULL)
+		return (NULL);
+
+	_strcpy(newDog->name, name);
+	_strcpy(newDog->owner, owner);
+	newDog->age = age;
+
+	return (newDog);
+}
