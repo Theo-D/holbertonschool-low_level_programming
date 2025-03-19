5f2b86f361daa1b031c93d852aff3f5991b7d799
Theo-D <theodessaigne@hotmail.fr>
2025-03-19 09:19:05 +0100

    Added 2-print_strings.c that contains the definition for print_strings() that print strings passed as args seperated by a string. Updated variadic_functions.h to include the declaration for print_strings(). Updated README?md with a descritpion for 2-print_strings.c

---
+++ variadic_functions/2-print_strings.c
@@ -0,0 +1,38 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ * print_numbers - prints numbers given as arguments, separated by a string.
+ * @separator: String used as separator between numbers.
+ * @n: number of arguments
+ *
+ * Return: nothing.
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list args;
+	unsigned int i;
+	char *s;
+
+	va_start(args, n);
+
+	for (i = 0; i < n; i++)
+	{
+		s = va_arg(args, char*);
+
+		if (separator == NULL)
+			separator = "";
+
+		if (s == NULL)
+			printf("(NIL)");
+		else 
+			printf("%s", s);
+
+		if (i != n - 1)
+			printf("%s", separator);
+	}
+	printf("\n");
+	va_end(args);
+}
