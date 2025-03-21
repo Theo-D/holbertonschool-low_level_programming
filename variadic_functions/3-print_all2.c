e8891baaee21fae836353184fc35e776783a3d1e
Theo-D <theodessaigne@hotmail.fr>
2025-03-21 09:03:43 +0100

    Addind copy of 3-print_all.c for testing purposes

---
+++ variadic_functions/3-print_all2.c
@@ -0,0 +1,85 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+/**
+ * printChar - Prints character from list.
+ * @args: The list from which printing the character.
+ *
+ * Return: Nothing.
+ */
+void printChar(va_list args)
+{
+	printf("%c", va_arg(args, int));
+}
+/**
+ * printInt - Prints integer from list.
+ * @args: The list from which printing the integer.
+ *
+ * Return: Nothing.
+ */
+void printInt(va_list args)
+{
+	printf("%d", va_arg(args, int));
+}
+/**
+ * printFloat - Prints float from list.
+ * @args: The list from which printing the float.
+ *
+ * Return: Nothing.
+ */
+void printFloat(va_list args)
+{
+	printf("%f", va_arg(args, double));
+}
+/**
+ * printString - Prints character from string.
+ * @args: The list from which printing the string.
+ *
+ * Return: Nothing.
+ */
+void printString(va_list args)
+{
+	char *string;
+
+	string = va_arg(args, char*);
+	if (string == NULL)
+		string = "(nil)";
+	printf("%s", string);
+}
+/**
+ * print_all - prints an arguments according to a format specifier.
+ * @format: string of format specifier for printing the argument.
+ *
+ * Return: Nothing
+ */
+void print_all(const char * const format, ...)
+{
+	va_list args;
+	int i = 0, j;
+	char *sep = "";
+        formSpec_t fS[] = {
+                {'c', printChar},
+                {'i', printInt},
+                {'f', printFloat},
+                {'s', printString},
+                {'\0', NULL}
+	};
+
+	va_start(args, format);
+	while (format[i]  && format)
+	{
+		j = 0;
+		while (fS[j].f)
+		{
+			if (format[i] == fS[j].f)
+			{
+				fS[j]._print(args);
+				sep = ", ";
+				printf("%s", sep);
+			}
+			j++;
+		}
+		i++;
+	}
+	printf("\n");
+	va_end(args);
+}
