3d8392dc19971ab15bf65658ddfc82c0208d4142
Theo-D <theodessaigne@hotmail.fr>
2025-03-31 13:11:51 +0200

    Added 0-print_list.c that contains the definition for print_list() that prints all the elements of a list_t list. Added lists.h that contains declaration for data structure list_t and for print_list(). Added README.md that contains a short description for the c files in this repo.

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+/**
+ * print_list - Prints all the elements of a list_t list.
+ * @h: List from which the elements are printed.
+ *
+ * Return: The number of nodes.
+ */
+size_t print_list(const list_t *h)
+{
+	int nodeCount = 0;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%u] (%s)\n", h->len, h->str);
+		h = h->next;
+		nodeCount++;
+	}
+	return (nodeCount);
+}
---
+++ singly_linked_lists/README.md
@@ -0,0 +1,3 @@
+0-print_list - Contains the definition for a function that prints all the elements of a list_t.
+
+
