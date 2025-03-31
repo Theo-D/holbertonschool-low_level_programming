9b3d9691828ebcc81c95c47c3c670742afcba9d2
Theo-D <theodessaigne@hotmail.fr>
2025-03-31 15:32:07 +0200

    Added description for 2-add_node.c

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -1,6 +1,11 @@
 #include "lists.h"
 /**
+ * add_node - Add a new node at the beginning of a list_t list.
+ * @head: Pointer to list to which the node should be prepended.
+ * @str: Str to be copied into string of new node.
  *
+ * Return: The adress of newly created node. If malloc fails,
+ * returns NULL.
  */
 list_t *add_node(list_t **head, const char *str)
 {
