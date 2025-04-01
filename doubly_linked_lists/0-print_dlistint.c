1a111e7ca0dd481432223958b8994fb295f552ef
Theo-D <theodessaigne@hotmail.fr>
2025-04-01 12:03:40 +0200

    Added 0-print_dlistint.c that contains the definition for print_dlistint(). Updated lists.h to include the declaration for print_dlistint.c. Added README.md tha contains a description for the c files in this repo.

---
+++ doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,34 @@
+#include "lists.h"
+/**
+ * print_dlistint - Prints all the elements of a dlistint_t list.
+ * @h: Pointer to the first node of the list_t list.
+ *
+ * Return: Cound of nodes from the list.
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	unsigned int nodeCount = 0;
+
+	if (h->prev == NULL)
+	{
+		while (h != NULL)
+		{
+			printf("%d\n", h->n);
+			h = h->next;
+			nodeCount++;
+		}
+		return (nodeCount);
+	}
+
+	if (h->next == NULL)
+	{
+		while (h != NULL)
+		{
+			printf("%d\n", h->n);
+			h = h->prev;
+			nodeCount++;
+		}
+		return (nodeCount);
+	}
+	return (0);
+}
