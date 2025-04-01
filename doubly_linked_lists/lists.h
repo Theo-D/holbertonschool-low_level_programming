a5e0de0f50ab0d3fa7e952bd725ecca21a4a77d6
Theo-D <theodessaigne@hotmail.fr>
2025-04-01 13:46:11 +0200

    Updated lists.h to include the declaration for dlistint_len()

--- doubly_linked_lists/lists.h
+++ doubly_linked_lists/lists.h
@@ -39,6 +39,7 @@ typedef struct dlistint_s
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 size_t print_dlistint(const dlistint_t *h);
+size_t dlistint_len(const dlistint_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
