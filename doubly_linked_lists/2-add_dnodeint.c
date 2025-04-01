da9b81e9b1e68914a052a39b7b880194ce98f08a
Theo-D <theodessaigne@hotmail.fr>
2025-04-01 14:45:48 +0200

    Changed code to take into account link to previous node

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -10,13 +10,22 @@
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *newNode = malloc(sizeof(dlistint_t));
+	dlistint_t *temp = NULL;
 
 	if (newNode == NULL)
 		return (NULL);
 
 	newNode->n = n;
-	newNode->next = *head;
 	newNode->prev = NULL;
+
+	if (*head == NULL)
+		newNode->next = NULL;
+	else
+	{
+		temp = *head;
+		temp->prev = newNode;
+		newNode->next = *head;
+	}
 	*head = newNode;
 
 	return (newNode);
