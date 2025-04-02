a0e30639511a4945fb3d5a294cf1c034085268d4
Theo-D <theodessaigne@hotmail.fr>
2025-04-02 15:39:14 +0200

    revert to previous state

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -30,9 +30,6 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	if (idx == 0)
 		newNode = add_dnodeint(h, n);
 
-	if (temp == NULL)
-		return NULL;
-
 	while (temp != NULL)
 	{
 		if (pos == idx - 1)
@@ -51,6 +48,8 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 	if (temp == NULL && pos == idx)
 		newNode = add_dnodeint_end(h, n);
+	if (temp == NULL)
+		newNode = NULL;
 
 	return (newNode);
 }
