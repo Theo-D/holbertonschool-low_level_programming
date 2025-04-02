adadfba3f99fd204b27a27e121ff9e60729fb36c
Theo-D <theodessaigne@hotmail.fr>
2025-04-02 09:43:15 +0200

    frees variables when malloc fails

--- doubly_linked_lists/3-add_dnodeint_end.c
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -11,7 +11,11 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	dlistint_t *newNode = malloc(sizeof(dlistint_t));;
 
 	if (temp == NULL)
+	{
+		free(temp);
+		free(newNode);
 		return (NULL);
+	}
 
 	newNode->n = n;
 	newNode->next = NULL;
