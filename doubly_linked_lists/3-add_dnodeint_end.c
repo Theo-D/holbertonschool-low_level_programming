87909c8f44c88a41e88bf20d899e2e1e2443fd68
Theo-D <theodessaigne@hotmail.fr>
2025-04-02 11:18:41 +0200

    Fixed memory leak by removing redundant malloc call

--- doubly_linked_lists/3-add_dnodeint_end.c
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -8,15 +8,11 @@
  */
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
-	dlistint_t *temp = malloc(sizeof(dlistint_t));
+	dlistint_t *temp;
 	dlistint_t *newNode = malloc(sizeof(dlistint_t));
 
-	if (temp == NULL || newNode == NULL)
-	{
-		free(temp);
-		free(newNode);
+	if (newNode == NULL)
 		return (NULL);
-	}
 
 	newNode->n = n;
 	newNode->next = NULL;
