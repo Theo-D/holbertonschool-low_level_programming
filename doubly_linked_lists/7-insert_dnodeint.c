d19df67ba5e9fcac3c15c05cf17242645fbce719
Theo-D <theodessaigne@hotmail.fr>
2025-04-02 15:23:10 +0200

    fixed variable name from last commit

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -48,7 +48,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 	if (temp == NULL && pos == idx)
 		newNode = add_dnodeint_end(h, n);
-	else if (current == NULL)
+	else if (temp == NULL)
 		return (NULL);
 
 	return (newNode);
