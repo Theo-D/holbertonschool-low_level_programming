ec23fa5cafae5bedcf1d7d10c629a97b4dd037f9
Theo-D <theodessaigne@hotmail.fr>
2025-04-02 09:46:59 +0200

    Completed condition for handling newNode malloc failing

--- doubly_linked_lists/3-add_dnodeint_end.c
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -2,15 +2,16 @@
 /**
  * add_dnodeint_end - Add a node at the end of a dlistint_t list.
  * @head: Pointer to the first element of the dlistint_t list.
+ * @n: Data of type integer to copy to new node.
  *
- * Return: A pointer to the created node or NULL if malloc fails. 
+ * Return: A pointer to the created node or NULL if malloc fails.
  */
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
 	dlistint_t *temp = malloc(sizeof(dlistint_t));
-	dlistint_t *newNode = malloc(sizeof(dlistint_t));;
+	dlistint_t *newNode = malloc(sizeof(dlistint_t));
 
-	if (temp == NULL)
+	if (temp == NULL || newNode == NULL)
 	{
 		free(temp);
 		free(newNode);
