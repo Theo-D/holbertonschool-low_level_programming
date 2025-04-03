06fb0a6a95f2910638e86d4d3c70ae201e0e3c59
Theo-D <theodessaigne@hotmail.fr>
2025-04-03 13:01:23 +0200

    Changed condition for inserting node at end of list

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -61,7 +61,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	temp->next = newNode;
 	newNode->prev = temp;
 
-	if (temp == NULL && pos == idx)
+	if (temp->next == NULL)
 		return (add_dnodeint_end(h, n));
 
 	return (newNode);
