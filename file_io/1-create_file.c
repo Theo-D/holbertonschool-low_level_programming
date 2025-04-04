728d1098b97e6dfef54017edabb945300d2851cd
Theo-D <theodessaigne@hotmail.fr>
2025-04-04 11:54:56 +0200

    fixed open() syntax

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -15,7 +15,7 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	fileDesc = open(filename, ORDWR | O_CREAT | O_TRUNC, 0600);
+	fileDesc = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
 
 	if (text_content == NULL)
 		return (1);
