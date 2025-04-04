063861ce838b8c223e1252f23c91239500f3e0b5
Theo-D <theodessaigne@hotmail.fr>
2025-04-04 11:52:17 +0200

    Changed flag orders within open()

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -15,18 +15,20 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	fileDesc = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	fileDesc = open(filename, ORDWR | O_CREAT | O_TRUNC, 0600);
 
 	if (text_content == NULL)
 		return (1);
 
 	if (fileDesc == -1)
 		return (-1);
-       	while (text_content[i] != 0)
+
+       	while (text_content[i])
 	{
 		textSize++;
 		i++;
 	}
+
 	if (i != 0)
 	{
 		writtenChar = write(fileDesc, text_content, textSize);
