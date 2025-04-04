c9e15ca687090d3d658877522ad70a47cc638f66
Theo-D <theodessaigne@hotmail.fr>
2025-04-04 11:48:26 +0200

    changed while loop position to avoid seg fault if text_content is NULL

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -15,12 +15,6 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	while (text_content[i] != 0)
-	{
-		textSize++;
-		i++;
-	}
-
 	fileDesc = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 
 	if (text_content == NULL)
@@ -28,7 +22,11 @@ int create_file(const char *filename, char *text_content)
 
 	if (fileDesc == -1)
 		return (-1);
-
+       	while (text_content[i] != 0)
+	{
+		textSize++;
+		i++;
+	}
 	if (i != 0)
 	{
 		writtenChar = write(fileDesc, text_content, textSize);
