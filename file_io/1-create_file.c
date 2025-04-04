793d1eb90a4ae059c83e74267fba8086602de540
Theo-D <theodessaigne@hotmail.fr>
2025-04-04 11:40:27 +0200

    Added condition for correct handling text content being NULL

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -12,7 +12,7 @@ int create_file(const char *filename, char *text_content)
 	ssize_t writtenChar = 0;
 	int i = 0, textSize = 0;
 
-	if (filename == NULL || text_content == NULL)
+	if (filename == NULL)
 		return (-1);
 
 	while (text_content[i] != 0)
@@ -22,6 +22,9 @@ int create_file(const char *filename, char *text_content)
 	}
 
 	fileDesc = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+
+	if (text_content == NULL)
+		return (1);
 
 	if (fileDesc == -1)
 		return (-1);
