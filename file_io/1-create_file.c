74642567dbfbc745a6cf57cd1d65b67b70306d02
Theo-D <theodessaigne@hotmail.fr>
2025-04-04 11:30:16 +0200

    Contains the definition for create_file() that creates a file

---
+++ file_io/1-create_file.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+ * create_file - Creates a file.
+ * @filename: Name of the file to be created
+ * @text_content: Text to be written to the file created.
+ *
+ * Return: 1 if function succeeds or -1 if cuntion fails.
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fileDesc = 0;
+	ssize_t writtenChar = 0;
+	int i = 0, textSize = 0;
+
+	if (filename == NULL)
+		return (-1);
+
+	while (text_content[i] != 0)
+	{
+		textSize++;
+		i++;
+	}
+
+	fileDesc = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
+
+	if (fileDesc == -1)
+		return (-1);
+
+	writtenChar = write(fileDesc, text_content, textSize);
+
+	if (writtenChar == -1)
+		return (-1);
+
+	return (1);
+}
