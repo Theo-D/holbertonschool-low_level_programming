165cc376f74ba23bd0d18d0bffaa0c1d007fd985
Theo-D <theodessaigne@hotmail.fr>
2025-04-04 11:37:23 +0200

    Changed flag order in open()

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -21,7 +21,7 @@ int create_file(const char *filename, char *text_content)
 		i++;
 	}
 
-	fileDesc = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
+	fileDesc = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 
 	if (fileDesc == -1)
 		return (-1);
